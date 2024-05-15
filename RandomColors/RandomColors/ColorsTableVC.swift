040ec077b6ef137ec4600de573240e19d15b8fbf
Furkan Şentürk <119443953+Sweezy0@users.noreply.github.com>
2024-05-15 14:19:12 +0300

    Add files via upload

---
+++ RandomColors/RandomColors/ColorsTableVC.swift
@@ -0,0 +1,55 @@
+//
+//  ColorsTableVC.swift
+//  RandomColors
+//
+//  Created by Furkan on 15.05.2024.
+//
+
+import UIKit
+
+class ColorsTableVC: UIViewController {
+    
+    var colors: [UIColor] = []
+    enum Cells{
+        static let colorCell = "ColorCell"
+    }
+    enum Segues{
+        static let toDetail = "toColorsDetailVC"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addRandomColors()
+
+        
+    }
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(.random())
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ColorsDetailVC
+        destinationVC.color = sender as? UIColor
+    }
+    
+}
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell) else{
+            return UITableViewCell()
+        }
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let color = colors[indexPath.row]
+        performSegue(withIdentifier: Segues.toDetail, sender: color)
+    }
+}
---
+++ RandomColors/RandomColors/UIColor + Ext.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor + Ext.swift
+//  RandomColors
+//
+//  Created by Furkan on 15.05.2024.
+//
+
+import UIKit
+
+extension UIColor{
+    static func random () -> UIColor{
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        return randomColor
+    }
+}
